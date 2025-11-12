c0b913ec7905b38227b3476aa004a01a0ae1d8fa
sj8259 <saijeevanofficial@gmail.com>
2025-11-12 11:22:29 +0530

    Remove unused import from EmailService

--- backend-spring/src/main/java/com/gamestack/service/EmailService.java
+++ backend-spring/src/main/java/com/gamestack/service/EmailService.java
@@ -2,7 +2,6 @@ package com.gamestack.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
